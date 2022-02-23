8f2975a9b06faf17598584847295fc0c785ace6a
Vxkapa <omefiiisto@gmail.com>
2022-02-23 23:03:05 +0000

    Userinfo update

--- src/commands/Information/userinfo.js
+++ src/commands/Information/userinfo.js
@@ -19,13 +19,27 @@ module.exports = class UserInfo extends Command {
 
 		const userI = message.guild.members.cache.get(USER.id);
 
+		const status = userI.presence.status
+			.replace('dnd', 'Ocupado')
+			.replace('idle', 'Ausente')
+			.replace('online', 'Disponível')
+			.replace('invisible', 'Invisível');
+
 		var nickname;
-		if (userI.nickname == null) {nickname = 'Sem nickname.';}
-		else {nickname = userI.nickname;}
+		if (userI.nickname == null) {
+			nickname = 'Sem nickname.';
+		}
+		else {
+			nickname = userI.nickname;
+		}
 
 		var boosted;
-		if (userI.premiumSinceTimestamp == null) {boosted = 'Não é boost.';}
-		else {boosted = `<t:${Math.floor(userI.premiumSinceTimestamp / 1e3)}:d>`;}
+		if (userI.premiumSinceTimestamp == null) {
+			boosted = 'Não é boost.';
+		}
+		else {
+			boosted = `<t:${Math.floor(userI.premiumSinceTimestamp / 1e3)}:d>`;
+		}
 
 		const joined = `<t:${Math.floor(userI.joinedAt / 1e3)}:d>`;
 		const created = `<t:${Math.floor(
@@ -33,17 +47,20 @@ module.exports = class UserInfo extends Command {
 		)}:d>`;
 
 		const UserInfo = new this.client.embed(message.author)
-			.setAuthor({ name: USER.tag, iconURL: USER.displayAvatarURL({ dynamic: true }) })
+			.setAuthor({
+				name: USER.tag,
+				iconURL: USER.displayAvatarURL({ dynamic: true }),
+			})
 			.addFields(
 				{
 					name: 'Informações Pessoais',
-					value: `${e.User} | Nome de Usuário: **${USER.username}**\n${e.ID} | ID: **${USER.id}**\n${e.Time} | Conta criada: **${created}**`,
-					inline: true
+					value: `${e.User} | Nome de Usuário: **${USER.username}**\n${e.ID} | ID: **${USER.id}**\n${e.Time} | Conta criada: **${created}**\n${e.On} | Status: **gu${status}**`,
+					inline: true,
 				},
 				{
 					name: 'Informações no Servidor',
 					value: `${e.Info} | Apelido: **${nickname}**\n${e.World} | Entrou em: **${joined}**\n${e.Archive} | Maior Cargo: **${userI.roles.highest}**\n${e.Crystal} | Booster: **${boosted}**`,
-					inline: true
+					inline: true,
 				}
 			)
 			.setThumbnail(USER.avatarURL({ dynamic: true, size: 2048 }));
