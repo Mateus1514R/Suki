67993c4c9250404b10604f5ff4b03a36daa4f848
Vxkapa <omefiiisto@gmail.com>
2022-02-24 00:26:15 +0000

    Pay command

---
+++ src/commands/Economy/pay.js
@@ -0,0 +1,48 @@
+const Command = require('../../structures/Command');
+const e = require('../../utils/Emojis');
+
+module.exports = class Daily extends Command {
+	constructor (client) {
+		super(client);
+		this.client = client;
+
+		this.name = 'pay';
+		this.category = 'Economy';
+		this.description = 'Envie dinheiro para um usuário.';
+		this.aliases = ['doar', 'pagar', 'enviar'];
+	}
+
+	async execute ({ message, args }) {
+
+		const user = await this.client.userDB.findOne({ _id: message.author.id });
+
+		const USER = this.client.getUser(args[0], message);
+		if(!USER) return message.reply(`${e.Error} | ${message.author}, você precisa inserir o usuário que deseja fazer o pagamento.`);
+		if(USER.id == message.author.id) return message.reply(`${e.Error} | ${message.author}, você não pode enviar dinheiro para si mesmo.`);
+
+		const alvoDB = await this.client.userDB.findOne({ _id: USER.id });
+
+		const money = parseInt(args[1]);
+		if(!money) return message.reply(`${e.Error} | ${message.author}, você precisa inserir a quantia de dinheiro que deseja enviar ao usuário.`);
+		if(isNaN(money)) return message.reply(`${e.Error} | ${message.author}, você precisa inserir uma quantia válida.`);
+		if(money <= 0) return message.reply(`${e.Error} | ${message.author}, a quantia deve ser maior que 0.`);
+		if(money > user.coins) return message.reply(`${e.Error} | ${message.author}, você não possui dinheiro suficiente para realizar o pagamento.`);
+
+		if(alvoDB) {
+			alvoDB.coins = alvoDB.coins + money;
+			await alvoDB.save();
+		}
+		else {
+			await this.client.userDB.create({
+				_id: USER.id,
+				coins: money,
+			});
+		}
+
+		user.coins = user.coins - money;
+		await user.save();
+
+		return message.reply(`${e.Correct} | ${message.author}, pagamento de **${money.toLocaleString()} coins** feito com sucesso para \`${USER.username}\`.`);
+
+	}
+};
