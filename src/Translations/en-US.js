9e479f365ff598b6d9cd836099bc92d5f0680571
Nicolas Ribeiro <psychoniskii2@gmail.com>
2022-02-25 18:40:46 -0300

    New translations en-US.js (English)

--- src/Translations/en-US.js
+++ src/Translations/en-US.js
@@ -1,5 +1,5 @@
 module.exports = {
-	lang: 'en',
+	lang: 'module.exports = {\n    lang: \'fas\',\n    messageCreate: {\n        mention: \'\n\',\n        mention2: \'\n\',\n        mention3: \'\n\'\n    }\n};',
 	messageCreate: {
 		mention: '',
 		mention2: '',
