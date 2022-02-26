2853bbb63cad88906531b735e413e8566ebb5e72
yMat3us_ <59369107+Mateus1514R@users.noreply.github.com>
2022-02-26 16:53:29 -0300

    Corrigindo bug

--- src/commands/Music/queue.js
+++ src/commands/Music/queue.js
@@ -1,136 +1,135 @@
 const { MessageButton, MessageActionRow } = require('discord.js');
-const Command = require('../../structures/Command')
+const Command = require('../../structures/Command');
+const Collection = require('../../structures/Collection')
 
 module.exports = class Queue extends Command {
 	constructor (client) {
-		super(client)
+		super(client);
 		this.client = client;
-		
-		this.collection = []
 
-		this.name = 'queue'
-		this.category = 'Music'
-		this.description = ''
-		this.aliases = ['']
+		this.name = 'queue';
+		this.category = 'Music';
+		this.description = '';
+		this.aliases = [''];
 	}
 
-	async execute ({ message }) {
+	async execute ({ message, lang }) {
 
-    const player = this.client.music.players.get(message.guild.id)
+		const player = this.client.music.players.get(message.guild.id);
 
-		if(!player) return message.reply('Não tem nada tocando neste servidor!')
+		if(!player) return message.reply(`${lang.commands.queue.noPlayer}`);
 
 		let pos = 1;
 
-    let page = 1;
+		let page = 1;
 
-    let pages = Math.ceil(this.collection.length / 1)
+		let pages = Math.ceil(this.collection.length / 1);
+		
+		const fila = new Collection()
 
-    player.queue.forEach((music) => {
+		player.queue.forEach((music) => {
 
-      this.collection.push(`${pos++}. [${music.title}](${music.uri}) - [${music.requester}]`)
+			fila.push(`${pos++}. [${music.title}](${music.uri}) - [${music.requester}]`);
 
-    })
+		});
 
-    const button_next = new MessageButton()
-    .setCustomId('Button_1')
-    .setLabel('Next')
-    .setStyle('SECONDARY')
+		const button_next = new MessageButton()
+			.setCustomId('Button_1')
+			.setLabel('Next')
+			.setStyle('SECONDARY');
 
-    const button_back = new MessageButton()
-    .setCustomId('Button_2')
-    .setLabel('Back')
-    .setStyle('SECONDARY')
+		const button_back = new MessageButton()
+			.setCustomId('Button_2')
+			.setLabel('Back')
+			.setStyle('SECONDARY');
 
-    if(pages <= 1) button_next.setDisabled(true)
+		if(pages <= 1) button_next.setDisabled(true);
 
-    const row = new MessageActionRow()
-    .addComponents(button_next, button_back)
+		const row = new MessageActionRow()
+			.addComponents(button_next, button_back);
 
-    const paginate = this.collection.slice((page - 1) * 10, page * 10)
+		const embed = new this.client.embed(message.author)
+			.setAuthor({ name: 'Queue', iconURL: message.guild.iconURL({}) })
+			.setDescription(`**${player.queue.length <= 0 ? `${lang.commands.queue.noMusics}` : fila.paginate(page, 10).join('\n')}\n\n${lang.commands.queue.music}: [${player.current.title}](${player.current.uri})**`);
 
-    const embed = new this.client.embed(message.author)
-    .setAuthor({ name: 'Queue', iconURL: message.guild.iconURL({}) })
-    .setDescription(`**${player.queue.length <= 0 ? 'Nenhuma música na minha fila' : this.collection.slice((page - 1) * 10, page * 10).join('\n')}\n\nMúsica tocando atualmente: [${player.current.title}](${player.current.uri})**`)
+		await message.reply({ embeds: [embed], components: [row] }).then((msg) => {
 
-    await message.reply({ embeds: [embed], components: [row] }).then((msg) => {
+			if(pages <= 1) return;
 
-      if(pages <= 1) return;
+			let filter = (inter) => {
 
-      let filter = (inter) => {
+				return inter.isButton() && inter.message.id === msg.id && inter.user.id === message.author.id;
 
-        return inter.isButton() && inter.message.id === msg.id && inter.user.id === message.author.id;
+			};
 
-      }
+			const collector = msg.channel.createMessageComponentCollector({ filter: filter, time: 60000 });
 
-      const collector = msg.channel.createMessageComponentCollector({ filter: filter, time: 60000 })
+			collector.on('collect', async (inter) => {
 
-      collector.on('collect', async(inter) => {
+				if(inter.customId === 'Button_1') {
 
-        if(inter.customId === 'Button_1') {
+					await inter.deferUpdate();
 
-          await inter.deferUpdate()
+					page++;
 
-          page++
+					if(!fila.paginate(page, 10).join('')) {
 
-          if(!this.collection.slice((page - 1) * 10, page * 10).join('')) {
+						page--;
 
-            page--
+						button_next.setDisabled(true);
 
-            button_next.setDisabled(true)
+						await msg.edit({ embeds: [embed], components: [row] });
 
-            await msg.edit({ embeds: [embed], components: [row] })
+						return;
 
-            return;
+					}
 
-          }
+					embed.setDescription(`**${fila.paginate(page, 10).join('\n')}\n\n${lang.commands.queue.music}: [${player.current.title}](${player.current.uri})**`);
 
-          embed.setDescription(`**${this.collection.slice((page - 1) * 10, page * 10).join('\n')}\n\nMúsica tocando atualmente: [${player.current.title}](${player.current.uri})**`)
+					button_back.setDisabled(false);
 
-          button_back.setDisabled(false)
+					await msg.edit({ embeds: [embed], components: [row] });
 
-          await msg.edit({ embeds: [embed], components: [row] })
+				}
 
-        }
+				if(inter.customId === 'Button_2') {
 
-        if(inter.customId === 'Button_2') {
+					await inter.deferUpdate();
 
-          await inter.deferUpdate()
+					page--;
 
-          page--
+					if(!fila.paginate(page, 10).join('')) {
 
-          if(!this.collection.slice((page - 1) * 10, page * 10).join('')) {
+						page++;
 
-            page++
+						button_back.setDisabled(true);
 
-            button_back.setDisabled(true)
+						await msg.edit({ embeds: [embed], components: [row] });
 
-            await msg.edit({ embeds: [embed], components: [row] })
+						return;
 
-            return;
+					}
 
-          }
+					embed.setDescription(`**${fila.paginate(page, 10).join('\n')}\n\n${lang.commands.queue.music}: [${player.current.title}](${player.current.uri})**`);
 
-          embed.setDescription(`**${this.collection.slice((page - 1) * 10, page * 10).join('\n')}\n\nMúsica tocando atualmente: [${player.current.title}](${player.current.uri})**`)
+					button_next.setDisabled(false);
 
-          button_next.setDisabled(false)
+					await msg.edit({ embeds: [embed], components: [row] });
 
-          await msg.edit({ embeds: [embed], components: [row] })
+				}
+			});
 
-        }
-      })
+			collector.on('end', async (r, reason) => {
 
-      collector.on('end', async(r, reason) => {
+				if(reason !== 'time') return;
 
-        if(reason !== 'time') return;
+				button_back.setDisabled(true);
+				button_next.setDisabled(true);
 
-        button_back.setDisabled(true)
-        button_next.setDisabled(true)
+				await msg.edit({ embeds: [embed.setFooter({ text: `${lang.commands.queue.time}` })], components: [row] });
 
-        await msg.edit({ embeds: [embed.setFooter({ text: 'O tempo acabou' })], components: [row] })
-
-      })
-      setTimeout(async() => { msg.delete() }, 120000)
-    })
-  }
-}
+			});
+			setTimeout(async () => { msg.delete(); }, 120000);
+		});
+	}
+};
