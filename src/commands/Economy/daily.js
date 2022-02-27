6f75703c5e41d7c1c8a5713a8a7763fc959fd6a2
Niskii3 <snitchblasta30@gmail.com>
2022-02-27 09:30:08 -0300

    fix daily command

--- src/commands/Economy/daily.js
+++ src/commands/Economy/daily.js
@@ -59,10 +59,8 @@ module.exports = class Daily extends Command {
 		const seconds = ~~(ms / 1000);
 		const minutes = ~~(seconds / 60);
 		const hours = ~~(minutes / 60);
-		const days = ~~(hours / 24);
 
 		return {
-		  days,
 		  hours: hours % 24,
 		  minutes: minutes % 60,
 		  seconds: seconds % 60,
